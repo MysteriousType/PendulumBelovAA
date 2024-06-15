3ffc39be41919ab3ea6c3f5aaf24e112a67ccaef
MysteriousType <s9moddata@gmail.com>
2024-06-15 22:20:04 +0300

    55

--- Assets/Logic/Runtime/GameContext.cs
+++ Assets/Logic/Runtime/GameContext.cs
@@ -1,10 +1,32 @@
 ﻿namespace Assets.Logic.Runtime
 {
+    using UnityEngine;
+
     public static class GameContext
     {
         public static void Initialize()
         {
+            InitializeQuality();
+            InitializeCamera();
+        }
 
+        private static void InitializeQuality()
+        {
+            const int FRAME_RATE_LOCK = 60;
+            Application.targetFrameRate = FRAME_RATE_LOCK;
+        }
+
+        private static void InitializeCamera()
+        {
+            Camera camera = Camera.main;
+
+            float w = 5;
+            float h = 10;
+            float x = w * 0.5f - 0.5f;
+            float y = h * 0.5f - 0.5f;
+
+            //camera.transform.position = new Vector3(x, y, -10f);
+            camera.orthographicSize = ((w > h * camera.aspect) ? (float)w / (float)camera.pixelWidth * camera.pixelHeight : h) / 2f;
         }
     }
 }
