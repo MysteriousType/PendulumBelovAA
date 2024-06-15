540ee065df817b4c52489d928c053ff7e7c4a6f5
Alexander Belov <73599587+MysteriousType@users.noreply.github.com>
2024-06-15 12:47:19 +0300

    initial (#1)
    Co-authored-by: MysteriousType <s9moddata@gmail.com>

---
+++ Assets/Logic/Runtime/GameLoader.cs
@@ -0,0 +1,19 @@
+﻿namespace Assets.Logic.Runtime
+{
+    using Assets.Logic.Runtime.Common;
+
+    public class GameLoader : BaseMonoBehaviourSingleton<GameLoader>
+    {
+        private protected override bool OnSingletonInitialized()
+        {
+            if (base.OnSingletonInitialized())
+            {
+                GameContext.Initialize();
+                DontDestroyOnLoad(this);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
