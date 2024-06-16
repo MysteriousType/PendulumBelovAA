dba2f3031d84c308de8a2f103e75a26369bbc528
MysteriousType <s9moddata@gmail.com>
2024-06-16 14:55:39 +0300

    44

---
+++ Assets/Logic/Runtime/Containers/ContainerTrigger.cs
@@ -0,0 +1,17 @@
+﻿namespace Assets.Logic.Runtime.Containers
+{
+    using Assets.Logic.Runtime.Balls;
+    using System;
+    using UnityEngine;
+
+    public class ContainerTrigger : MonoBehaviour
+    {
+        public event Action<Ball> OnBallEntered;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            Ball ball = collision.GetComponent<Ball>();
+            OnBallEntered?.Invoke(ball);
+        }
+    }
+}
--- Assets/Logic/Runtime/Containers/ContainersManager.cs
+++ Assets/Logic/Runtime/Containers/ContainersManager.cs
@@ -7,8 +7,26 @@
         private const int MATRIX_SIZE = 3;
         private readonly Ball[,] Balls = new Ball[MATRIX_SIZE, MATRIX_SIZE];
 
-        private void OnBallLanded(Ball ball, int columnIndex)
+        private ContainerTrigger _topTrigger;
+        private ContainerTrigger _centerTrigger;
+        private ContainerTrigger _bottomTrigger;
+
+        public ContainersManager()
         {
+            InitializeTriggers();
+        }
+
+        private void InitializeTriggers()
+        {
+            _topTrigger.OnBallEntered += OnBallLanded;
+            _centerTrigger.OnBallEntered += OnBallLanded;
+            _bottomTrigger.OnBallEntered += OnBallLanded;
+        }
+
+        private void OnBallLanded(Ball ball)
+        {
+            int columnIndex = GetColumnIndexByXPosiiton(ball.transform.position.x);
+
             for (int rowIndex = 0; rowIndex < MATRIX_SIZE; rowIndex++)
             {
                 if (Balls[columnIndex, rowIndex] != null)
@@ -17,6 +35,28 @@
                     break;
                 }
             }
+        }
+
+        private int GetColumnIndexByXPosiiton(float xPosition)
+        {
+            if (xPosition > -1.074f && xPosition < -0.36f)
+            {
+                return 0;
+            }
+
+            if (xPosition > -0.36f && xPosition < 0.36f)
+            {
+                return 1;
+            }
+
+            /*
+            if (xPosition > 0.36f && xPosition < 1.074f)
+            {
+                return 2;
+            }
+            */
+
+            return 2;
         }
     }
 }
